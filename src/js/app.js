50290ef3b2a228011127c9d9993e9a63fd3402bb
Keith Wedinger <keith.wedinger@improving.com>
2018-02-16 11:36:34 -0500

    Added test to build task, fixed module error inside app

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,8 @@
 // and to enable extraction as CSS and injection of style into index.html
 import "../assets/scss/app.scss";
 import numeral from "numeral";
-import moduleExample from "./moduleExample";
+import { greeting } from "./moduleExample";
 
 const courseValue = numeral(1000).format("$0,0.00");
 console.log(`I would like to pay ${courseValue} for this course`); // eslint-disable-line no-console
-console.log(`Greeting = ${moduleExample().greeting()}`); // eslint-disable-line no-console
+console.log(`Greeting = ${greeting()}`); // eslint-disable-line no-console
